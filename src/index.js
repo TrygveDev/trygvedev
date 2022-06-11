11a1e48802bf0ff699a8b8bcef3e25f04b5cf4ca
TrygveDev <trygvemf@gmail.com>
2022-06-11 21:16:50 +0200

    init

---
+++ src/index.js
@@ -0,0 +1,21 @@
+document.getElementById('githubBtn').addEventListener('click', () => {
+    window.open("https://github.com/TrygveDev?tab=repositories", "_blank");
+})
+document.getElementById('jsBtn').addEventListener('click', () => {
+    window.open("https://www.w3schools.com/js/", "_blank");
+})
+document.getElementById('cssBtn').addEventListener('click', () => {
+    window.open("https://www.w3schools.com/css/", "_blank");
+})
+document.getElementById('htmlBtn').addEventListener('click', () => {
+    window.open("https://www.w3schools.com/html/", "_blank");
+})
+document.getElementById('elecBtn').addEventListener('click', () => {
+    window.open("https://www.electronjs.org/", "_blank");
+})
+document.getElementById('reacBtn').addEventListener('click', () => {
+    window.open("https://reactjs.org/", "_blank");
+})
+document.getElementById('fireBtn').addEventListener('click', () => {
+    window.open("https://firebase.google.com/", "_blank");
+})
