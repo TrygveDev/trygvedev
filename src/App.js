e15bc2a36dc310051d557acd1164581a526f175a
Trygve <trygvemf@gmail.com>
2023-02-28 14:55:19 +0100

    1.1 - Preset CSS

--- src/App.js
+++ src/App.js
@@ -1,9 +1,52 @@
+import { useEffect } from 'react';
 import styles from './styles/App.module.css';
 
 function App() {
+
+  useEffect(() => {
+    document.querySelectorAll(".App_card__Fv4Im").forEach(card => {
+      card.onmousemove = (e) => {
+        const { currentTarget: target } = e;
+        const rect = target.getBoundingClientRect(),
+          x = e.clientX - rect.left,
+          y = e.clientY - rect.top;
+
+        target.style.setProperty("--mouse-x", `${x}px`);
+        target.style.setProperty("--mouse-y", `${y}px`);
+      }
+    })
+  })
+
+
   return (
     <div className={styles.container}>
-
+      <h1>trygve.dev</h1>
+      <div className={styles.cardContainer}>
+        <div className={styles.card}>
+          <div className={styles.cardBorder}></div>
+          <div className={styles.cardContent}></div>
+        </div>
+        <div className={styles.card}>
+          <div className={styles.cardBorder}></div>
+          <div className={styles.cardContent}></div>
+        </div>
+        <div className={styles.card}>
+          <div className={styles.cardBorder}></div>
+          <div className={styles.cardContent}></div>
+        </div>
+        <div className={styles.card}>
+          <div className={styles.cardBorder}></div>
+          <div className={styles.cardContent}></div>
+        </div>
+        <div className={styles.card}>
+          <div className={styles.cardBorder}></div>
+          <div className={styles.cardContent}></div>
+        </div>
+        <div className={styles.card}>
+          <div className={styles.cardBorder}></div>
+          <div className={styles.cardContent}></div>
+        </div>
+      </div>
     </div>
   );
 }
